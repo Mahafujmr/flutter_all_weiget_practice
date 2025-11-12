9e7d04e4ef25896a7bbfbd3e9bb780a9094d6b7a
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-11-13 01:19:02 +0600

    flutter stack widget explain

--- lib/layout_widgets/stack_widget.dart
+++ lib/layout_widgets/stack_widget.dart
@@ -14,23 +14,76 @@ class StackWidget extends StatelessWidget {
         ),
         centerTitle: true,
       ),
+      //=> Stack Widget example
+      // body: Container(
+      //   width: double.infinity,
+      //   height: 500,
+      //   color: Colors.purple,
+      //   child: Stack(
+      //     alignment: Alignment.center,
+      //     children: [
+      //       Positioned(
+      //         left: 50,
+      //         child: Container(width: 300, height: 250, color: Colors.green),
+      //       ),
+      //       Container(width: 270, height: 200, color: Colors.yellow),
+      //       Container(width: 200, height: 150, color: Colors.red),
+      //     ],
+      //   ),
+      // ),
+
+      //=> another use case flutter stack widget
+      // body: Center(
+      //   child: Stack(
+      //     clipBehavior: Clip.none,
+      //     // alignment not work , cause use clip.none
+      //    //alignment: Alignment.center,
+      //     children: [
+      //       Container(width: 250, height: 300, color: Colors.green[400]),
+      //       Positioned(
+      //         top: -100,
+      //         right: 30,
+      //         child: Container(width: 200, height: 200, color: Colors.red),
+      //       ),
+      //       Positioned(
+      //         bottom: -25,
+      //         left: 60,
+      //         child: Container(width: 70, height: 50, color: Colors.yellow),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: Container(
         width: double.infinity,
-        height: 500,
-        color: Colors.purple,
+        height: 400,
+        color: Colors.grey,
         child: Stack(
-          alignment: Alignment.center,
+          // use expand - last widget show this properties
+          //fit: StackFit.expand,
+          //=> Control Text widget
+          textDirection: TextDirection.rtl,
           children: [
-            Positioned(
-              left: 50,
-
-              child: Container(width: 300, height: 250, color: Colors.green),
+            Container(width: 300, height: 250, color: Colors.red),
+            Container(
+              width: 230,
+              height: 100,
+              color: Colors.yellow,
+              child: Text('This is Stack WIdget'),
             ),
-            Container(width: 270, height: 200, color: Colors.yellow),
-            Container(width: 200, height: 150, color: Colors.red),
+            // Container(width: 120, height: 70, color: Colors.black),
+            Positioned(
+              right: 10,
+              bottom: 10,
+              child: Container(width: 100, height: 50, color: Colors.green),
+            ),
           ],
         ),
       ),
     );
   }
 }
+/*
+Stack widget important part
+positioned widget (top,bottom, left, write)
+valu (-) so any widget outside 
+*/
