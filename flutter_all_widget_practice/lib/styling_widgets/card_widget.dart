350683d647f6a537d91dd24ddb8e83764472ea79
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-07-29 22:09:18 +0600

    flutter complete card widget

--- flutter_all_widget_practice/lib/styling_widgets/card_widget.dart
+++ flutter_all_widget_practice/lib/styling_widgets/card_widget.dart
@@ -11,17 +11,9 @@ class CardWidget extends StatelessWidget {
             backgroundColor: Colors.amber,
         ),
         body: Padding(
-          padding: const EdgeInsets.all(17.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
-              Card(
-                   elevation: 5,
-                  surfaceTintColor: Colors.amber,
-                  color: Colors.red,
-                  shadowColor: Colors.red,
-                  child: Image(image: AssetImage("images/laptop.jpeg"),),
-                      ),
-                      SizedBox(height: 20,),
                        Card(
                    elevation: 10,
                   surfaceTintColor: Colors.amber,
@@ -36,13 +28,44 @@ class CardWidget extends StatelessWidget {
           
                   Card(
                     elevation: 10,
-                    shadowColor: Colors.red,
+                    shadowColor: Colors.yellow,
+                    
                     child: Container(
                       width: double.infinity,
-                      height: 200,
+                      height: 150,
                       color: Colors.blue,
                     ),
-                  )   
+                  ) ,
+                  SizedBox(height: 15,)  ,
+                  Card(
+                    borderOnForeground: false,
+                    clipBehavior: Clip.antiAlias,
+                    elevation: 20,
+                    shadowColor: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(100.0),),
+                      side: BorderSide(
+                        color: Colors.red,width: 20,
+                       // strokeAlign:0.7,
+                        style: BorderStyle.solid,
+                      )
+                    ),
+                    child: Image.asset("images/quran.jpeg",width: 300,),
+                  ),
+                  Card(
+                    color: Colors.orange,
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40)
+
+                      ),
+                    ),
+                    child: Text("Dart Programme",style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                  )
             ],
           ),
         ),
