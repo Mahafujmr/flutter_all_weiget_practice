d9ccae7ef8de1e61279ea600f896bd4bc830a059
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-09-08 14:43:45 +0600

    flutter page view widget overview

--- flutter_all_widget_practice/lib/scrolling_widget/pase_view/pase_view_widget.dart
+++ flutter_all_widget_practice/lib/scrolling_widget/pase_view/pase_view_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_all_widget_practice/scrolling_widget/grid_view/grid_view_builder.dart';
+import 'package:flutter_all_widget_practice/scrolling_widget/list_view/listView_builder_widget.dart';
 
 class PaseViewWidget extends StatefulWidget {
   const PaseViewWidget({super.key});
@@ -6,11 +8,17 @@ class PaseViewWidget extends StatefulWidget {
   @override
   State<PaseViewWidget> createState() => _PaseViewWidgetState();
 }
+final PageController _controller = PageController(
+    initialPage: 0
+  );
 
-final _controller = PageController(
-  initialPage: 0,
-);
 class _PaseViewWidgetState extends State<PaseViewWidget> {
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -19,20 +27,81 @@ class _PaseViewWidgetState extends State<PaseViewWidget> {
         centerTitle: true,
         backgroundColor: Colors.yellow,
       ),
-      body: PageView(
-        controller: _controller,
+      body: Column(
         children: [
-          Container(
-            color: Colors.deepOrange,
-          ),
-          Container(
-            color: Colors.black,
-          ),
+          Expanded(
+            child:PageView(
+              controller: _controller,
+        children: [
+          GridViewBuilder(),
+          ListviewBuilderWidget(),
           Container(
             color: Colors.orange,
           ),
         ],
       ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+        ),
+        onPressed: () {
+        _controller.nextPage(
+          duration:Duration(milliseconds: 200) , 
+          curve: Curves.easeInOut,
+          );
+      }, child:Text("Next Screen",
+      style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+      ) 
+      ),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+        ),
+        onPressed: () {
+        _controller.previousPage(
+          duration:Duration(milliseconds: 200) , 
+          curve: Curves.easeInOut,
+          );
+      }, child:Text("Previous Screen",
+      style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+      ) 
+      ),
+      Flexible(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+          ),
+          onPressed: () {
+          _controller.jumpToPage(2);
+        }, child:Text("Other  Screen",
+        style: TextStyle(color: Colors.black,fontSize: 13),
+        ) 
+        ),
+      ),
+      Flexible(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+          ),
+          onPressed: () {
+          _controller.animateToPage(3,
+           duration: Duration(milliseconds: 200), 
+           curve: Curves.easeInQuart,
+           );
+        }, child:Text("Animated",
+        style: TextStyle(color: Colors.black,fontSize: 13),
+        ) 
+        ),
+      ),
+        ],
+      )
+        ],
+
+      )
     );
   }
 }
